138172ef8ac92cfe6112d6667012d03363c82487
VechorkoTetyana <44574436+VechorkoTetyana@users.noreply.github.com>
2024-04-29 11:22:01 +0200

    Add files via upload

---
+++ JobHuntTV/JobHuntTV/ProductCell/ProductTableViewCell.swift
@@ -0,0 +1,13 @@
+import Foundation
+import UIKit
+
+class ProductTableViewCell: UITableViewCell {
+    
+    
+    
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    override class func awakeFromNib() {}
+}
